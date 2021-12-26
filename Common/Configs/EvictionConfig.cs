575649b21a009a82c4ef71b9524e4e07a71ee5ad
Michael Scott <creeper37500@gmail.com>
2021-12-25 21:41:10 -0500

    1.0
    Released mod.

---
+++ Common/Configs/EvictionConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Terraria.ModLoader.Config;
+
+namespace NPCFlagsAlways.Common.Configs
+{
+	public class EvictionConfig : ModConfig
+	{
+		public override ConfigScope Mode => ConfigScope.ClientSide;
+
+		[DefaultValue(false)]
+		[DisplayName("Eviction Always Allowed")]
+		[Tooltip("If true, then right-clicking an NPC banner will evict them even if the housing menu is closed.")]
+		public bool EvictionAllowed { get; set; }
+	}
+}
